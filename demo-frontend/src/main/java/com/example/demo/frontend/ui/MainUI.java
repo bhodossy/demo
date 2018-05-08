b378fffb42951090d6385eee81f683d8938f738f
Denes Berta <mr.denes.berta@gmail.com>
2018-05-08 09:05:17 +0100

    candidate UI

--- demo-frontend/src/main/java/com/example/demo/frontend/ui/MainUI.java
+++ demo-frontend/src/main/java/com/example/demo/frontend/ui/MainUI.java
@@ -1,11 +1,5 @@
 package com.example.demo.frontend.ui;
 
-import com.example.demo.backend.publication.DefaultPublicationService;
-import com.example.demo.model.base.dao.BaseDao;
-import com.example.demo.model.context.AppCtx;
-import com.example.demo.model.profile.Profile;
-import com.example.demo.model.profile.student.entity.Student;
-import com.example.demo.model.publication.entity.Publication;
 import com.vaadin.annotations.Theme;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.ui.*;
@@ -33,27 +27,82 @@ public class MainUI extends UI {
 //      setContent(new Label(AppCtx.bean(DefaultPublicationService.class).getAuthorFromPublication().toString()));
     }
 
+    private GridLayout pubGrid = new GridLayout(2,1);
+
     private void candidateStart() {
-        setContent(new Label("here we start a candidate enquiry"));
+        VerticalLayout candidateLayout = new VerticalLayout();
+        candidateLayout.setDefaultComponentAlignment(Alignment.TOP_CENTER);
+        candidateLayout.setWidth("100%");
+        HorizontalLayout dates = new HorizontalLayout();
+        dates.setWidth("300pt");
+
+        DateField startDate = new DateField();
+        DateField endDate = new DateField();
+
+        TextField fNameField = new TextField("First Name");
+        TextField lNameField = new TextField("Last Name");
+        TextField keywordsField = new TextField("Fields of expertise");
+        keywordsField.setPlaceholder("history, linear algebra");
+        keywordsField.setWidth("300pt");
+        Label datesLabel = new Label("Availability");
+        Button addPublication = new Button("Add publication");
+        addPublication.addClickListener(event -> pubPopUp());
+
+        pubGrid.addComponent(new Label("Title"), 0, 0);
+        pubGrid.addComponent(new Label("DOI"), 1, 0);
+        pubGrid.setWidth("300pt");
+
+        dates.addComponents(startDate, endDate);
+        dates.setComponentAlignment(startDate, Alignment.TOP_LEFT);
+        dates.setComponentAlignment(endDate, Alignment.TOP_RIGHT);
+        candidateLayout.addComponents(fNameField, lNameField, keywordsField, datesLabel, dates, addPublication, pubGrid);
+        setContent(candidateLayout);
+    }
+
+    private TextField title = new TextField("Title");
+    private TextField doi = new TextField("DOI");
+    private TextField keys = new TextField("Keywords");
+    private TextField abs = new TextField("Abstract");
+    private Window subWindow = new Window("Add publication");
+
+    private void pubPopUp() {
+        VerticalLayout subContent = new VerticalLayout();
+        subWindow.center();
+
+        HorizontalLayout options = new HorizontalLayout();
+        Button save = new Button("Save");
+        save.addClickListener(event -> addPub());
+        Button cancel = new Button("Cancel");
+        cancel.addClickListener(e -> subWindow.close());
+        options.addComponents(save, cancel);
+
+        subContent.addComponents(title, doi, keys, abs, options);
+        subWindow.setContent(subContent);
+        addWindow(subWindow);
+    }
+
+    private void addPub() {
+        pubGrid.insertRow(1);
+        pubGrid.addComponents(new Label(title.getValue()), new Label(doi.getValue()));
+        subWindow.close();
     }
 
     private void piStart() {
         VerticalLayout layout = new VerticalLayout();
         layout.setWidth("100%");
+        layout.setDefaultComponentAlignment(Alignment.TOP_CENTER);
+        HorizontalLayout options = new HorizontalLayout();
+
         TextField freeform = new TextField();
         freeform.setPlaceholder("example: postdoc in molecular biology");
         freeform.setWidth("80%");
-        layout.addComponent(freeform);
-        layout.setComponentAlignment(freeform, Alignment.TOP_CENTER);
-        HorizontalLayout options = new HorizontalLayout();
         Button submit = new Button("Submit");
         submit.addClickListener(event -> freeQuery(freeform.getValue()));
         Button filter = new Button("Search by filters");
         filter.addClickListener(event -> startFilter());
-        options.addComponent(submit);
-        options.addComponent(filter);
-        layout.addComponent(options);
-        layout.setComponentAlignment(options, Alignment.TOP_CENTER);
+
+        options.addComponents(submit, filter);
+        layout.addComponents(freeform, options);
         setContent(layout);
     }
 
